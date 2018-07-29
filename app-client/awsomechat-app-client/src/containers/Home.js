1e37ae059273a47ffd50d0af09273cb091ee83d3
Vic Cooper <viccooper@Vics-MacBook-Pro.local>
2018-07-29 16:44:39 -0700

    home page

---
+++ app-client/awsomechat-app-client/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Awsomechat</h1>
+          <p>Take Charge of your Chat</p>
+        </div>
+      </div>
+    );
+  }
+}
